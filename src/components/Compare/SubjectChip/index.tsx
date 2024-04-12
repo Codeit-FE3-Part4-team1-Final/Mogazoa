221ad9767df6eb728fbb0a1b74b700fd06445c93
Crack_Egg <jaemyung123@naver.com>
2024-04-12 15:52:21 +0900

    ✨ Feat: SubjectChip에 대한 기초 구조 세팅

--- src/components/Compare/SubjectChip/index.tsx
+++ src/components/Compare/SubjectChip/index.tsx
@@ -2,4 +2,15 @@ interface SubjectChipInterface {
   name: string;
 }
 
-export default function SubjectChip({ name }: SubjectChipInterface) {}
+export default function SubjectChip({ name }: SubjectChipInterface) {
+  return (
+    <>
+      <div>
+        {name}
+        <div>
+          <div />
+        </div>
+      </div>
+    </>
+  );
+}
