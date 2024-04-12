80c517219c0d289960f08edbe00649f15f94ff53
Crack_Egg <jaemyung123@naver.com>
2024-04-12 16:25:09 +0900

    ✨ Feat: ObjectChip 컴포넌트 생성

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -1,3 +1,11 @@
+import ObjectChip from '@/components/Compare/ObjectChip';
+import SubjectChip from '@/components/Compare/SubjectChip';
+
 export default function ComparePage() {
-  return <div>ComparePage</div>;
+  return (
+    <div>
+      <SubjectChip name={'비교 상품'} />
+      <ObjectChip name={'비교 상품'} />
+    </div>
+  );
 }
