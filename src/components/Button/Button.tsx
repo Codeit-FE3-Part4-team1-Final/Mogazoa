ce12df3cf9d412de7956b4ff65aff31466d13f88
siyeol97 <leesi2830@naver.com>
2024-04-12 14:11:10 +0900

    💄 Feat: Button 공통 컴포넌트 제작

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import styles from './Button.module.scss';
+
+interface Props {
+  children: ReactNode;
+  width?: string;
+  height: string;
+  category: 'primary' | 'secondary' | 'tertiary';
+}
+
+export default function Button({
+  children,
+  width = '100%',
+  height,
+  category,
+}: Props) {
+  const buttonClass = `${styles.button} ${styles[category]}`;
+
+  return (
+    <button className={buttonClass} style={{ width, height }}>
+      {children}
+    </button>
+  );
+}
