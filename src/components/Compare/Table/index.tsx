e2f64183abc6f9607da508597fe17c476c3aca57
Crack_Egg <jaemyung123@naver.com>
2024-04-13 14:39:24 +0900

    ✨ Feat: 테이블 컴포넌트에 필요한 함수 구현

--- src/components/Compare/Table/index.tsx
+++ src/components/Compare/Table/index.tsx
@@ -1,4 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from 'react';
+import Result from './Result';
+import TableCompare from './TableCompare';
+
 interface TableInterface {
-  SubejctProduct: any;
+  SubjectProduct: any;
   ObjectProduct: any;
+}
+
+export default function Table({
+  SubjectProduct,
+  ObjectProduct,
+}: TableInterface) {
+  const [count, setCount] = useState(0);
+  const [victoryProduct, setVictoryProduct] = useState('');
+  const handleCount = (value: number) => setCount(value);
+  const showVictoryProduct = (value: string) => setVictoryProduct(value);
+
+  return (
+    <>
+      <div>
+        <Result count={count} victoryProduct={victoryProduct} />
+        <span>3가지 항목 중 {count}가지 항목에서 우세합니다.</span>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>기준</th>
+            <th>상품 1</th>
+            <th>상품 2</th>
+            <th>결과</th>
+          </tr>
+        </thead>
+        <TableCompare
+          handleCount={handleCount}
+          showVictoryProduct={showVictoryProduct}
+          SubjectProduct={SubjectProduct}
+          ObjectProduct={ObjectProduct}
+        />
+      </table>
+    </>
+  );
 }
