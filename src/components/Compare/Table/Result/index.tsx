a4b8bd5d0e7295a2efff5af363872d7ab6fc1931
Crack_Egg <jaemyung123@naver.com>
2024-04-13 14:26:11 +0900

    ✨ Feat: 결과 메세지 보여주는 함수 구현

--- src/components/Compare/Table/Result/index.tsx
+++ src/components/Compare/Table/Result/index.tsx
@@ -2,3 +2,13 @@ interface ResultInterface {
   count: number;
   victoryProduct: string;
 }
+
+export default function Result({ count, victoryProduct }: ResultInterface) {
+  return count === 0 ? (
+    <div>무승부입니다.</div>
+  ) : (
+    <div>
+      <span>{victoryProduct}</span> 상품이 승리하였습니다.
+    </div>
+  );
+}
