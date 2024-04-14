d0177fb78a8edeb40ede65fa4f131968cb812a7d
SongSang <941227_@naver.com>
2024-04-14 18:29:26 +0900

    💄 :style:상단 네비게이션바 반응형 UI 완료

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,10 +1,21 @@
 import Image from 'next/image';
 import styles from './index.module.scss';
 
+// Todo(송상훈) : UI만 그려놓음, 로그인 기능 후 로직 완성 예정
 export default function NavigationBar() {
   return (
     <div className={styles.container}>
-      <Image src='/images/logo.svg' alt='로고 이미지' width={166} height={28} />
+      <div className={styles['menu-icon']}>
+        <Image
+          src='/images/menu-icon.svg'
+          alt='메뉴 이미지'
+          width={24}
+          height={24}
+        />
+      </div>
+      <div className={styles['logo-wrapper']}>
+        <Image src='/images/logo-L.svg' alt='로고 이미지' fill />
+      </div>
       <div className={styles['navigation-item']}>
         <div className={styles.search}>
           <Image
@@ -13,7 +24,10 @@ export default function NavigationBar() {
             width={24}
             height={24}
           />
-          <input placeholder='상품 이름을 검색해 보세요' />
+          <input
+            className={styles[`search-input`]}
+            placeholder='상품 이름을 검색해 보세요'
+          />
         </div>
         <div className={styles['navigation-user-menu']}>
           <p>로그인</p>
