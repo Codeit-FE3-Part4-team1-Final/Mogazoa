5552128c9b65d76a055a9da01257f026b8ecfdee
Crack_Egg <jaemyung123@naver.com>
2024-04-15 22:43:29 +0900

    ✨ Feat: 로컬스토리지에 상품 1에 대한 정보가 있을 때 로직 처리

--- src/components/Input/SubjectInput/index.tsx
+++ src/components/Input/SubjectInput/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface SubjectInputInterface {
   handleUpdate: (name: string) => void;
@@ -9,10 +9,21 @@ export default function SubjectInput({
   handleUpdate,
   handleClose,
 }: SubjectInputInterface) {
-  const [subjectProduct, setSubejctProduct] = useState<string>('');
+  const [subjectProduct, setSubjectProduct] = useState<string>('');
   const [subjectProductId, setSubjectProductId] = useState<number>(0);
   const [subjectChip, setSubjectChip] = useState<string>('');
   const [isShow, setIsShow] = useState<boolean>(false);
   const [isChip, setIsChip] = useState<boolean>(false);
   const [isReadable, setIsReadable] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (localStorage.getItem('subjectProduct')) {
+      setSubjectProduct(localStorage.getItem('subjectProduct') as string);
+      setSubjectProductId(Number(localStorage.getItem('subjectProductId')));
+      setSubjectChip(localStorage.getItem('subjectProduct') as string);
+      setIsChip(true);
+      setSubjectProduct('');
+      setIsReadable(true);
+    }
+  }, []);
 }
