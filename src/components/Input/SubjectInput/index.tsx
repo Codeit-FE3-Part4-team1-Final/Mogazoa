2faa7f885f5a9dae8ee46c12ca94e12a255e8bb0
Crack_Egg <jaemyung123@naver.com>
2024-04-15 21:42:50 +0900

    ✨ Feat: 기초 인풋 상품 초기화 세팅

--- src/components/Input/SubjectInput/index.tsx
+++ src/components/Input/SubjectInput/index.tsx
@@ -1,4 +1,18 @@
+import { useState } from 'react';
+
 interface SubjectInputInterface {
   handleUpdate: (name: string) => void;
   handleClose: () => void;
+}
+
+export default function SubjectInput({
+  handleUpdate,
+  handleClose,
+}: SubjectInputInterface) {
+  const [subjectProduct, setSubejctProduct] = useState<string>('');
+  const [subjectProductId, setSubjectProductId] = useState<number>(0);
+  const [subjectChip, setSubjectChip] = useState<string>('');
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const [isChip, setIsChip] = useState<boolean>(false);
+  const [isReadable, setIsReadable] = useState<boolean>(false);
 }
