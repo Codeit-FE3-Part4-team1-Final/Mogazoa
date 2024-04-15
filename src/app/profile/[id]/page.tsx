6f4e9eb150c1e4ebfee9757c1137dcf595fd1183
siyeol97 <leesi2830@naver.com>
2024-04-15 16:52:00 +0900

    💄 Fix: 반응형 스타일 정의된 mixin 반영

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,10 +1,16 @@
+import Button from '@/components/Button';
 import ActivityCard from '@/components/Card/ActivityCard';
+import CategoryChip from '@/components/Chip/CategoryChip';
 
 export default function page() {
   return (
     <div>
       프로필 페이지
       <ActivityCard />
+      <Button height='60px' category='primary'>
+        버튼
+      </Button>
+      <CategoryChip productCategory='음악' size='large' />
     </div>
   );
 }
