9484e19cdffb743d43724c24e07d9bfe64f4f34a
jobible <whtjdrud800@gmail.com>
2024-04-15 19:42:20 +0900

    Revert ":recycle: Refactor: cx 분리"
    This reverts commit 5f2ce05d112bf2701208d731e2429f99ed14adf7.

--- src/components/Thumbs/index.tsx
+++ src/components/Thumbs/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Image from 'next/image';
-import cx from '@/constants/cx.ts';
+import cx from '@/components/Thumbs/cx.ts';
 
 interface Props {
   likeCount: number;
