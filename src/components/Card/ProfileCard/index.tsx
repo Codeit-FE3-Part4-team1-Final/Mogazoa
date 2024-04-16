fc4cc319dd0672061cc0686ae3268c4f0ecdc57a
siyeol97 <leesi2830@naver.com>
2024-04-16 15:07:24 +0900

    💄 Fix: ProfileCard 컴포넌트 height 수정

--- src/components/Card/ProfileCard/index.tsx
+++ src/components/Card/ProfileCard/index.tsx
@@ -20,7 +20,11 @@ export default function ProfileCard() {
           <span className={cx('user-name')}>surisuri마수리</span>
           <p className={cx('user-explain')}>
             세상에 리뷰 못할 제품은 없다. surisuri마수리와 함께라면 당신도
-            프로쇼핑러! 안녕하세요, 별점의 화신 surisuri마수리입니다!
+            프로쇼핑러! 안녕하세요, 별점의 화신 surisuri마수리입니다! 세상에
+            리뷰 못할 제품은 없다. surisuri마수리와 함께라면 당신도 프로쇼핑러!
+            안녕하세요, 별점의 화신 surisuri마수리입니다! 세상에 리뷰 못할
+            제품은 없다. surisuri마수리와 함께라면 당신도 프로쇼핑러!
+            안녕하세요, 별점의 화신 surisuri마수리입니다!
           </p>
         </div>
         <div className={cx('user-follow-box')}>
