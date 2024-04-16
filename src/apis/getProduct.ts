9fc020d9cbe4cb9d231a339b398e8dafe46974ba
Crack_Egg <jaemyung123@naver.com>
2024-04-16 20:46:59 +0900

    ✨ Feat: (임시) axios 라이브러리 설치 및 method 타입 정의

--- src/apis/getProduct.ts
+++ src/apis/getProduct.ts
@@ -1,3 +1,12 @@
+import { AxiosRequestConfig } from 'axios';
+
+const httpMethod = {
+  GET: 'get',
+  POST: 'post',
+  DELETE: 'delete',
+  PATCH: 'patch',
+} as const;
+
 interface getProductInterface {
   keyword?: string | null;
   category?: number | null;
@@ -6,4 +15,20 @@ interface getProductInterface {
   page?: number;
 }
 
-const getProduct = {};
+interface requestInterface<U = any> {
+  method: (typeof httpMethod)[keyof typeof httpMethod];
+  endpoint: string;
+  data?: U;
+  config?: AxiosRequestConfig;
+}
+
+const getProduct = ({
+  keyword,
+  category,
+  order,
+  cursor,
+  page,
+}: getProductInterface) => {
+  const queryParams = `?${keyword ? `&keyword=${keyword}` : ''}${category ? `&category=${category}` : ''}${order ? `&order=${order}` : ''}${cursor ? `&cursor=${cursor}` : ''}`;
+  const request: 
+};
