99beb80821f30d4efa6317687f5b0badb2fd5b32
Crack_Egg <jaemyung123@naver.com>
2024-04-16 21:53:30 +0900

    ✨ Feat: 비교하기 페이지 인풋 컴포넌트 적용

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -1,19 +1,42 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import CompareInput from '@/components/Input/CompareInput';
+'use client';
 
-interface ComparePageInterface {
-  subjectProduct: any;
-  objectProduct: any;
-  handleUpdate: (name: string) => void;
-  handleClose: () => void;
-}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from 'react';
+import CompareInput from '@/components/Input/CompareInput';
+import Table from '@/components/Table';
 
 export default function ComparePage() {
+  const [subjectProduct, setSubjectProduct] = useState<any>();
+  const [objectProduct, setObjectProduct] = useState<any>();
+  const [isShow, setIsShow] = useState<boolean>(false);
+
+  const handleSubject = (data: string) => {
+    setSubjectProduct(data);
+  };
+
+  const handleObject = (data: string) => {
+    setObjectProduct(data);
+  };
+
+  const handleClose = () => setIsShow(false);
+
   return (
     <>
       <div>
-        <CompareInput isSubject={true} handleUpdate={} handleClose={} />
+        <CompareInput
+          isSubject={true}
+          handleUpdate={handleSubject}
+          handleClose={handleClose}
+        />
+        <CompareInput
+          isSubject={false}
+          handleUpdate={handleObject}
+          handleClose={handleClose}
+        />
       </div>
+      {isShow && (
+        <Table SubjectProduct={subjectProduct} ObjectProduct={objectProduct} />
+      )}
     </>
   );
 }
