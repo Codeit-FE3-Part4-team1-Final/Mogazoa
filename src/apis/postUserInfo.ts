4ff554620fcbb27c56ced81d0123758b62b1396a
SongSang <941227_@naver.com>
2024-04-20 02:41:49 +0900

    ✨ :feat: sign 페이지 라우팅 처리 및 회원가입 로직 구현

--- src/apis/postUserInfo.ts
+++ src/apis/postUserInfo.ts
@@ -5,14 +5,31 @@ interface SignInData {
   password: string;
 }
 
-const signInUser = async ({ data }: { data: SignInData }) => {
+interface SignUpData {
+  email: string;
+  nickname: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+// todo(송상훈) : 토스트나 모달로 실패 메세지 처리하기
+
+export const signInUser = async ({ data }: { data: SignInData }) => {
   const res = await axiosPostJson('auth/signIn', data);
-  if (!res.status) {
-    console.log(res);
+  if (res.user) {
     localStorage.setItem('accessToken', res.accessToken);
-  } else {
-    console.log(res.data.message);
+    return true;
   }
+  console.log(res.data.message);
+  return false;
 };
 
-export default signInUser;
+export const signUpUser = async ({ data }: { data: SignUpData }) => {
+  const res = await axiosPostJson('auth/signUp', data);
+  if (res.user) {
+    localStorage.setItem('accessToken', res.accessToken);
+    return true;
+  }
+  console.log(res.data.message);
+  return false;
+};
