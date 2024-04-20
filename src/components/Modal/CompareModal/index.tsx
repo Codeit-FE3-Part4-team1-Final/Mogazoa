6c0d9c52b765fe8c7441647f654908956f510423
Crack_Egg <jaemyung123@naver.com>
2024-04-20 20:34:16 +0900

    ✨ Feat: 모달에 필요한 상태값 초기 설정

--- src/components/Modal/CompareModal/index.tsx
+++ src/components/Modal/CompareModal/index.tsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+
 type CompareModalType = 'subject' | 'object' | 'copy' | 'changed';
 
 interface CompareModalInterface {
   product: string;
   productId: number;
   compareModalType: CompareModalType;
+}
+
+export default function CompareModal({
+  product,
+  productId,
+  compareModalType,
+}: CompareModalInterface) {
+  const [subjectProduct, setSubjectProduct] = useState<string>('');
+  const [isSubjectSelected, setIsSubjectSelected] = useState<boolean>(false);
+  const [objectProduct, setObjectProduct] = useState<string>('');
+  const [isObjectSelected, setIsObjectSelected] = useState<boolean>(false);
+  const [isChanged, setIsChanged] = useState<boolean>(false);
+  const [messsage, setMessage] = useState<string>('');
 }
