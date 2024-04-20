7e14ba7c5e266f22a93c45ecda6bb1da7168a6f1
Crack_Egg <jaemyung123@naver.com>
2024-04-20 16:19:52 +0900

    💄 Style: 로딩 상태 애니메이션 구현

--- src/components/Loading/CompareLoading/index.tsx
+++ src/components/Loading/CompareLoading/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import classNames from 'classnames/bind';
 import styles from './CompareLoading.module.scss';
 
@@ -7,14 +6,12 @@ const cx = classNames.bind(styles);
 export default function CompareLoading() {
   return (
     <div className={cx('container')}>
-      <div className={cx('image-box')}>
-        <Image
-          src='/images/loading-L.svg'
-          alt='로딩 아이콘'
-          fill
-          className={cx('image')}
-        />
-      </div>
+      <div className={cx('circle')}></div>
+      <div className={cx('circle')}></div>
+      <div className={cx('circle')}></div>
+      <div className={cx('shadow')}></div>
+      <div className={cx('shadow')}></div>
+      <div className={cx('shadow')}></div>
     </div>
   );
 }
