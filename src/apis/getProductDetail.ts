5d4157860d38de8a1081e77641ba1e74304f2d34
Crack_Egg <jaemyung123@naver.com>
2024-04-20 15:37:30 +0900

    ✨ Feat: getProductDetail 타입 재정의

--- src/apis/getProductDetail.ts
+++ src/apis/getProductDetail.ts
@@ -1,52 +1,12 @@
-import { RequestMethodInterface } from '@/types/types';
+import { ProductDetailType, RequestMethodInterface } from '@/types/types';
 import { requestMethod } from '@/utils/fetchUtils';
 
-type CategoryType =
-  | '음악'
-  | '영화/드라마'
-  | '강의/책'
-  | '호텔'
-  | '가구/인테리어'
-  | '식당'
-  | '전자기기'
-  | '화장품'
-  | '의류/잡화'
-  | '앱';
-
-interface CategoryMetricInterface {
-  favoriteCount: number;
-  rating: number;
-  reviewCount: number;
-}
-
-interface CategoryInterface {
-  id: number;
-  name: CategoryType;
-}
-
-interface DetailInterface {
-  id: number;
-  name: string;
-  image: string;
-  rating: number;
-  reviewCount: number;
-  categoryId: number;
-  categoryMetric: CategoryMetricInterface;
-  createdAt: string;
-  updatedAt: string;
-  writerId: number;
-  description: string;
-  category: CategoryInterface;
-  isFavorite: boolean;
-  favoriteCount: number;
-}
-
-const getProductDetail = (productId: number): Promise<DetailInterface> => {
+const getProductDetail = (productId: number): Promise<ProductDetailType> => {
   const requestApi: RequestMethodInterface = {
     method: 'get',
     endPoint: `/products/${productId}`,
   };
-  return requestMethod<DetailInterface>(requestApi);
+  return requestMethod<ProductDetailType>(requestApi);
 };
 
 export default getProductDetail;
