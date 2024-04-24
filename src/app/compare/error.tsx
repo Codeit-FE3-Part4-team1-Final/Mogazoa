de229f8697f233802efd8d133a1f898c63d9c468
Crack_Egg <jaemyung123@naver.com>
2024-04-24 12:42:51 +0900

    💄 Style: 에러 페이지 스타일링 추가

--- src/app/compare/error.tsx
+++ src/app/compare/error.tsx
@@ -1,19 +1,23 @@
-export default function ComparePageError({
-  reset,
-  error,
-}: {
-  error: Error;
-  reset: () => void;
-}) {
-  console.log(error.message);
+'use client';
+
+import classNames from 'classnames/bind';
+import styles from './ComparePageError.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function ComparePageError({ reset }: { reset: () => void }) {
   return (
-    <div>
-      <div>
-        <div>
+    <div className={cx('container')}>
+      <div className={cx('message-container')}>
+        <div className={cx('header')}>
           <h2>알 수 없는 오류가 발생했습니다.</h2>
         </div>
-        <div>
-          <button type='button' onClick={() => reset()}>
+        <div className={cx('button-container')}>
+          <button
+            type='button'
+            onClick={() => reset()}
+            className={cx('button')}
+          >
             새로고침
           </button>
         </div>
