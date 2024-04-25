4520276f12a5222d65328b65e4ccb238e7fab487
SongSang <941227_@naver.com>
2024-04-25 18:53:49 +0900

    💄 :style:홈페이지 ui 반응형 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,58 @@
 import classNames from 'classnames/bind';
 import SideBar from '@/components/SideBar';
 import styles from './page.module.scss';
+import productItems from './test.ts';
+import { ProductCard } from '@/components/Card/ProductCard';
 
 export default function Home() {
   const cx = classNames.bind(styles);
 
   return (
-    <div className={cx('home-container')}>
-      <aside className={cx('aside')}>
-        <SideBar />
-      </aside>
-      <main className={cx('main')}>메인 정보 구역</main>
-      <section className={cx('ranking-section')}>랭킹 정보 구역</section>
+    <div className={cx('home-wrapper')}>
+      <div className={cx('home-container')}>
+        <aside className={cx('sidebar')}>
+          <SideBar />
+        </aside>
+        <main className={cx('main')}>
+          <section className={cx('item-section')}>
+            <p className={cx('item-header')}>
+              지금 <span className={cx('item-header-point')}>HOT</span> 상품
+            </p>
+            <div className={cx('itemList')}>
+              {productItems.map((productItem, index) => (
+                <div className={cx('item')}>
+                  <ProductCard key={index} productItem={productItem} />
+                </div>
+              ))}
+            </div>
+          </section>
+          <section className={cx('item-section')}>
+            <p className={cx('item-header')}>
+              별점 <span className={cx('item-header-point')}>TOP</span> 상품
+            </p>
+            <div className={cx('itemList')}>
+              {productItems.map((productItem, index) => (
+                <div className={cx('item')}>
+                  <ProductCard key={index} productItem={productItem} />
+                </div>
+              ))}
+            </div>
+          </section>
+          <section className={cx('item-section')}>
+            <p className={cx('item-header')}>
+              <span className={cx('item-header-point')}>NEW </span>등록 상품
+            </p>
+            <div className={cx('itemList')}>
+              {productItems.map((productItem, index) => (
+                <div className={cx('item')}>
+                  <ProductCard key={index} productItem={productItem} />
+                </div>
+              ))}
+            </div>
+          </section>
+        </main>
+        <section className={cx('ranking-section')}>리뷰어 랭킹</section>
+      </div>
     </div>
   );
 }
