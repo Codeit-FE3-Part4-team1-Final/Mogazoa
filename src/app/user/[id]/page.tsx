85deda08467f543ef37ae9340f0730507de3a141
siyeol97 <leesi2830@naver.com>
2024-04-25 18:53:42 +0900

    ✨ Feat: 로그인 상태일 때 내 프로필 페이지로 리다이렉트

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,12 +1,24 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import ProfileSection from '@/components/Profile/ProfileSection';
 import { UserDetail } from '@/types/types';
-import { getUserDetail } from '@/utils/getUserDetail';
+import { getMyDetail, getUserDetail } from '@/utils/getUserDetail';
 
 interface Props {
   params: { id: string };
 }
 
 export default async function UserProfilePage({ params: { id } }: Props) {
+  const token = cookies().get('accessToken');
   const userDetail: UserDetail = await getUserDetail(id);
+
+  if (token) {
+    const myDetail = await getMyDetail(token.value);
+    const myId = myDetail.id.toString();
+    if (id === myId) {
+      redirect('/mypage');
+    }
+  }
+
   return <ProfileSection userId={id} userDetail={userDetail} />;
 }
