ff76fe3caae5368bb8183f4a2ffc3bba9690cc02
SongSang <941227_@naver.com>
2024-04-25 19:05:18 +0900

    머지 후 수정

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -11,51 +11,53 @@ export default function NavigationBar() {
   const isLoggedIn = !!accessToken;
 
   return (
-    <div className={styles.container}>
-      <div className={styles['menu-icon']}>
-        <Image
-          src='/images/menu-icon.svg'
-          alt='메뉴 이미지'
-          width={24}
-          height={24}
-        />
-      </div>
-      <div className={styles['logo-wrapper']}>
-        <Image src='/images/logo-L.svg' alt='로고 이미지' fill />
-      </div>
-      <div className={styles['navigation-item']}>
-        <div className={styles.search}>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles['menu-icon']}>
           <Image
-            src='/images/search-icon.svg'
-            alt='돋보기 이미지'
+            src='/images/menu-icon.svg'
+            alt='메뉴 이미지'
             width={24}
             height={24}
           />
-          <input
-            className={styles[`search-input`]}
-            placeholder='상품 이름을 검색해 보세요'
-          />
         </div>
-        <div className={styles['navigation-user-menu']}>
-          {isLoggedIn ? (
-            <>
-              <Link href='/compare'>
-                <button className={styles.button}>비교하기</button>
-              </Link>
-              <Link href='/mypage'>
-                <button className={styles.button}>내프로필</button>
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link href='/signin'>
-                <button className={styles.button}>로그인</button>
-              </Link>
-              <Link href='/signup'>
-                <button className={styles.button}>회원가입</button>
-              </Link>
-            </>
-          )}
+        <div className={styles['logo-wrapper']}>
+          <Image src='/images/logo-L.svg' alt='로고 이미지' fill />
+        </div>
+        <div className={styles['navigation-item']}>
+          <div className={styles.search}>
+            <Image
+              src='/images/search-icon.svg'
+              alt='돋보기 이미지'
+              width={24}
+              height={24}
+            />
+            <input
+              className={styles[`search-input`]}
+              placeholder='상품 이름을 검색해 보세요'
+            />
+          </div>
+          <div className={styles['navigation-user-menu']}>
+            {isLoggedIn ? (
+              <>
+                <Link href='/compare'>
+                  <button className={styles.button}>비교하기</button>
+                </Link>
+                <Link href='/mypage'>
+                  <button className={styles.button}>내프로필</button>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link href='/signin'>
+                  <button className={styles.button}>로그인</button>
+                </Link>
+                <Link href='/signup'>
+                  <button className={styles.button}>회원가입</button>
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
