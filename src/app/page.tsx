3865a006ee3b6fe9e8c6c9300eba063d30f9c3b6
SongSang <941227_@naver.com>
2024-04-26 23:38:16 +0900

    ✨ :feat:홈페이지 main 데이터 연결 사이드바 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,85 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import SideBar from '@/components/SideBar';
 import styles from './page.module.scss';
 import ProductCard from '@/components/Card/ProductCard';
-import productItems from './test.ts';
+import getProduct from '@/apis/getProduct.ts';
+import { Category } from '@/types/types';
 
 export default function Home() {
   const cx = classNames.bind(styles);
+
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(
+    null,
+  );
+
+  const { data: hotProducts } = useQuery({
+    queryKey: ['hotProducts'],
+    queryFn: () => getProduct({ order: 'reviewCount' }),
+  });
+
+  const { data: topProducts } = useQuery({
+    queryKey: ['topProducts'],
+    queryFn: () => getProduct({ order: 'rating' }),
+  });
+
+  const { data: newProducts } = useQuery({
+    queryKey: ['newProducts'],
+    queryFn: () => getProduct({ order: 'recent' }),
+  });
 
   return (
     <div className={cx('home-wrapper')}>
       <div className={cx('home-container')}>
         <aside className={cx('sidebar')}>
-          <SideBar />
+          <SideBar setSelectedCategory={setSelectedCategory} />
         </aside>
         <main className={cx('main')}>
-          <section className={cx('item-section')}>
-            <p className={cx('item-header')}>
-              지금 <span className={cx('item-header-point')}>HOT</span> 상품
-            </p>
-            <div className={cx('itemList')}>
-              {productItems.map((productItem, index) => (
-                <div className={cx('item')}>
-                  <ProductCard key={index} productItem={productItem} />
+          {selectedCategory === null ? (
+            <div>
+              <section className={cx('item-section')}>
+                <p className={cx('item-header')}>
+                  지금 <span className={cx('item-header-point')}>HOT</span> 상품
+                </p>
+                <div className={cx('itemList')}>
+                  {hotProducts?.list.map((productItem) => (
+                    <div className={cx('item')} key={productItem.id}>
+                      <ProductCard productItem={productItem} />
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          </section>
-          <section className={cx('item-section')}>
-            <p className={cx('item-header')}>
-              별점 <span className={cx('item-header-point')}>TOP</span> 상품
-            </p>
-            <div className={cx('itemList')}>
-              {productItems.map((productItem, index) => (
-                <div className={cx('item')}>
-                  <ProductCard key={index} productItem={productItem} />
+              </section>
+              <section className={cx('item-section')}>
+                <p className={cx('item-header')}>
+                  별점 <span className={cx('item-header-point')}>TOP</span> 상품
+                </p>
+                <div className={cx('itemList')}>
+                  {topProducts?.list?.map((productItem) => (
+                    <div className={cx('item')} key={productItem.id}>
+                      <ProductCard productItem={productItem} />
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          </section>
-          <section className={cx('item-section')}>
-            <p className={cx('item-header')}>
-              <span className={cx('item-header-point')}>NEW </span>등록 상품
-            </p>
-            <div className={cx('itemList')}>
-              {productItems.map((productItem, index) => (
-                <div className={cx('item')}>
-                  <ProductCard key={index} productItem={productItem} />
+              </section>
+              <section className={cx('item-section')}>
+                <p className={cx('item-header')}>
+                  <span className={cx('item-header-point')}>NEW </span>등록 상품
+                </p>
+                <div className={cx('itemList')}>
+                  {newProducts?.list?.map((productItem) => (
+                    <div className={cx('item')} key={productItem.id}>
+                      <ProductCard productItem={productItem} />
+                    </div>
+                  ))}
                 </div>
-              ))}
+              </section>
             </div>
-          </section>
+          ) : (
+            <div>{selectedCategory.id}</div>
+          )}
         </main>
         <section className={cx('ranking-section')}>리뷰어 랭킹</section>
       </div>
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,19 +1,28 @@
 import classNames from 'classnames/bind';
+import styles from './SideBar.module.scss';
 import getCategories from '@/apis/getCategories';
 import { Category } from '@/types/types';
-import styles from './SideBar.module.scss';
 
-export default async function SideBar() {
+interface Props {
+  setSelectedCategory: (category: Category | null) => void;
+}
+
+// todo(송상훈) : 에러처리 로딩처리 추가할것
+export default function SideBar({ setSelectedCategory }: Props) {
   const cx = classNames.bind(styles);
 
-  const categories: Category[] = await getCategories();
+  const categories = getCategories();
 
   return (
     <div className={cx('sideBar-container')}>
       <h3 className={cx('sideBar-header')}>카테고리</h3>
       <ul className={cx('sideBar-list-container')}>
-        {categories.map((category) => (
-          <button className={cx('sideBar-list')} key={category.id}>
+        {categories?.map((category) => (
+          <button
+            className={cx('sideBar-list')}
+            key={category.id}
+            onClick={() => setSelectedCategory(category)}
+          >
             {category.name}
           </button>
         ))}
