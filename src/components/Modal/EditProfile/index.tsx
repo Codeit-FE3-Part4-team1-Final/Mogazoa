2e1f3612c60eb09a1dfd21877e42ccfaa0e8e9c1
siyeol97 <leesi2830@naver.com>
2024-04-27 23:54:18 +0900

    💄 Style: 프로필 편집 모달 UI 제작

--- src/components/Modal/EditProfile/index.tsx
+++ src/components/Modal/EditProfile/index.tsx
@@ -1,8 +1,26 @@
+'use client';
+
+import { ChangeEvent, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './EditProfile.module.scss';
+import Button from '@/components/Button';
 
 const cx = classNames.bind(styles);
 
 export default function EditProfile() {
-  return <div className={cx('wrapper')}>EditProfile</div>;
+  const [userName, setUserName] = useState('');
+  const handleChangeUserName = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  };
+
+  return (
+    <div className={cx('wrapper')}>
+      <span className={cx('title')}>프로필 편집</span>
+      <form className={cx('edit-profile-form')}>
+        <input type='file' />
+        <input type='text' value={userName} onChange={handleChangeUserName} />
+      </form>
+      <Button category='primary'>저장하기</Button>
+    </div>
+  );
 }
