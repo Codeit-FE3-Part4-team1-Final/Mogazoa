49125100910e89d4aaa6e09aaae72ae1f4c2f3e8
siyeol97 <leesi2830@naver.com>
2024-04-29 13:36:01 +0900

    💄 Feat: TextFieldInput 컴포넌트 제작

--- src/components/Modal/EditProfile/index.tsx
+++ src/components/Modal/EditProfile/index.tsx
@@ -4,6 +4,7 @@ import { ChangeEvent, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './EditProfile.module.scss';
 import Button from '@/components/Button';
+import TextFieldInput from '@/components/Input/TextFieldInput';
 
 const cx = classNames.bind(styles);
 
@@ -12,13 +13,21 @@ export default function EditProfile() {
   const handleChangeUserName = (e: ChangeEvent<HTMLInputElement>) => {
     setUserName(e.target.value);
   };
+  const handleOnBlur = () => {
+    console.log('blur');
+  };
 
   return (
     <div className={cx('wrapper')}>
       <span className={cx('title')}>프로필 편집</span>
       <form className={cx('edit-profile-form')}>
         <input type='file' />
-        <input type='text' value={userName} onChange={handleChangeUserName} />
+        <TextFieldInput
+          onBlur={handleOnBlur}
+          value={userName}
+          onChange={handleChangeUserName}
+          placeholder='닉네임을 입력해 주세요'
+        />
       </form>
       <Button category='primary'>저장하기</Button>
     </div>
