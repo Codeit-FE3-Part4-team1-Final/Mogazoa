a8ec6bd97d5641b0377b42876d57dc8b630d2d0c
jobible <whtjdrud800@gmail.com>
2024-04-30 10:25:27 +0900

    :sparkles: feat: 주소 공유하기 버튼

--- src/components/Production/index.tsx
+++ src/components/Production/index.tsx
@@ -55,6 +55,18 @@ export default function Production({ productData }: Readonly<Props>) {
     setIsOpen(true);
   };
 
+  const copyToClipboard = () => {
+    const currentUrl = window.location.href;
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText(currentUrl)
+        .then(() => alert('URL이 클립보드에 복사되었습니다.'))
+        .catch((err) => alert(`복사에 실패했습니다. 에러: ${err}`));
+    } else {
+      alert('이 브라우저에서는 클립보드 복사 기능을 지원하지 않습니다.');
+    }
+  };
+
   return (
     <div className={cx('container')}>
       <div className={cx('image-wrap', 'col-sm-4', 'col-md-4', 'col-lg-4')}>
@@ -85,7 +97,7 @@ export default function Production({ productData }: Readonly<Props>) {
             <button className={cx('btn')}>
               <Image src={'/images/kakao-icon.svg'} alt={'카카오아이콘'} fill />
             </button>
-            <button className={cx('btn')}>
+            <button className={cx('btn')} onClick={copyToClipboard}>
               <Image src={'/images/share-icon.svg'} alt={'카카오아이콘'} fill />
             </button>
           </div>
