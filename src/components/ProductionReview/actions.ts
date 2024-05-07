7d3d616346ecc236a505c121cb42b769cd06a7bc
jobible <whtjdrud800@gmail.com>
2024-05-08 00:47:40 +0900

    :bug:  Fix: accessToken이 없어도 리뷰를 불러올수 있게 수정

--- src/components/ProductionReview/actions.ts
+++ src/components/ProductionReview/actions.ts
@@ -5,21 +5,21 @@ import { cookies } from 'next/headers';
 const cookieStore = cookies();
 const accessToken = cookieStore.get('accessToken');
 
-export default async function getReview(productId: string, order: string) {
-  if (!accessToken) {
-    console.error('Access token is missing');
-    return null;
-  }
-
+export default async function getReview(productId: number, order: string) {
   try {
+    const headers: Record<string, string> = {
+      'Content-Type': 'application/json',
+    };
+
+    if (accessToken) {
+      headers.Authorization = `Bearer ${accessToken.value}`;
+    }
+
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL_HOST}/products/${productId}/reviews?order=${order}`,
       {
         method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken.value}`,
-        },
+        headers,
       },
     );
 
