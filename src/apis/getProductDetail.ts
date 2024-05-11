9f54e61668119685a71d1cb030ced4fb44951614
Crack_Egg <jaemyung123@naver.com>
2024-05-11 20:42:26 +0900

    ✨ Feat: productId 0값 오류 수정

--- src/apis/getProductDetail.ts
+++ src/apis/getProductDetail.ts
@@ -1,7 +1,12 @@
+/* eslint-disable prefer-promise-reject-errors */
 import { ProductDetailType, RequestMethodInterface } from '@/types/types';
 import { requestMethod } from '@/utils/fetchUtils';
 
 const getProductDetail = (productId: number): Promise<ProductDetailType> => {
+  if (productId === 0) {
+    return Promise.reject('Default ProductId');
+  }
+
   const requestApi: RequestMethodInterface = {
     method: 'get',
     endPoint: `products/${productId}`,
