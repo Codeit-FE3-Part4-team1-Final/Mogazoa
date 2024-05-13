6d6e98c1fd568a7a0be1ef10dc9f97122f39b58a
siyeol97 <leesi2830@naver.com>
2024-05-13 14:49:23 +0900

    🩹 Fix: vercel build 에러 해결

--- src/utils/SigninKakao.ts
+++ src/utils/SigninKakao.ts
@@ -1,8 +1,8 @@
-const signinToKakao = () => {
+const signToKakao = () => {
   const uri = `${process.env.NEXT_PUBLIC_SIGNIN_REDIRECT_URI}`;
   window.Kakao.Auth.authorize({
     redirectUri: uri,
   });
 };
 
-export default signinToKakao;
+export default signToKakao;
